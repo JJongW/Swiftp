613b6f54c9e4e7d9df5e19f45fe12328343bbb82
JJongW <71203867+JJongW@users.noreply.github.com>
2022-07-27 07:52:58 +0900

    Updown file
    check

---
+++ Updown/Updown/UpdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpdownApp.swift
+//  Updown
+//
+//  Created by JW on 2022/07/21.
+//
+
+import SwiftUI
+
+@main
+struct UpdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
