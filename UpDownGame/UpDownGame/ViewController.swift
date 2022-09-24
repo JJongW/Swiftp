f8994e05b75264474b4585057fe2f6b8f225e646
JJongW <shinjw4675@gmail.com>
2022-09-25 00:19:39 +0900

    9/25 3차까지 완료

--- UpDownGame/UpDownGame/ViewController.swift
+++ UpDownGame/UpDownGame/ViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        slider.setThumbImage(UIImage(named: "slider_thumb"), for: .normal)
+    }
+    
+    
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider){
+        print(sender.value)
+    }
+    @IBAction func touchUpHitButton(_ sender: UIButton){
+        print(slider.value)
+    }
+    @IBAction func touchUpResetButton(_ sender: UIButton){
+        print("print RESEEET!")
     }
 
 
